5f99527e19fedbd521da55e23ac85752edcdca30
Kamil Abzalov <mail@kamil-abzalov.ru>
2020-12-01 22:02:58 +0300

    styles refactoring

--- src/app/public/albums/albums.component.spec.ts
+++ src/app/public/albums/albums.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AlbumsComponent } from './albums.component';
 
@@ -6,7 +6,7 @@ describe('AlbumsComponent', () => {
   let component: AlbumsComponent;
   let fixture: ComponentFixture<AlbumsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AlbumsComponent ]
     })
